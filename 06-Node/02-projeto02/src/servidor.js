72825708ecd872ba9e1e70533b1c2cae6e0dd245
Diego Schmitz <diego.sctar@gmail.com>
2021-04-01 03:03:48 -0300

    terminando a parte de node, de como ele funciona no back-end, feito um projeto de exemplo, lendo um json de uma api, e criado uma api rest com crud basico, usando express

---
+++ 06-Node/02-projeto02/src/servidor.js
@@ -0,0 +1,43 @@
+const porta = 3003
+
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const bancoDeDados = require('./bancoDados')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/produtos', (req, res, nest) => {
+    //res.send({ nome: 'Notebook', preco: 124.65 }) //o send ja converte para json
+    res.send(bancoDeDados.getProdutos())
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto) // JSON
+})
+
+app.put('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto) // JSON
+})
+
+app.delete('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.excluirProduto(req.params.id)
+    res.send(produto) // JSON
+})
+
+app.listen(porta, () => {
+    console.log(`executando na porta ${porta}.`)
+})
